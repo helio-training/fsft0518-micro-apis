c01511d678dc5dd0b2b4f661a3e145277b48c3b2
Tyler Garlick <tgarlick@heliotraining.com>
2018-06-18 14:20:13 -0600

    Move to babel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const { send, json } = require('micro')
-const { router, get, post } = require('microrouter')
+import {send,json } from 'micro'
+import { router, get, post } from 'microrouter'
 
 let legos = [
   { name: "President Business", price: 9.99, isActive: true}
 ]
 
-module.exports = router(
+
+export default router(
   get('/', async (req, res) => await send(res, 200, legos)),
   post('/', async (req, res) => {
     // throw Error('Blah')
